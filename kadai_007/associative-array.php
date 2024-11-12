16a059c0d589c18adf184cd479e0ea534efa4b74
SHINNK-KS <izumiyou963@gmail.com>
2024-11-12 21:45:12 +0900

    kadai_007 upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>kadai_007</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      // 連想配列を作成
+      $product_id = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      //　出力する
+      print_r($product_id)
+      ?>
+    </p>
+  </body>
+</html>
